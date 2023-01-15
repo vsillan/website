8747ba170bb16a4c531004e6978462efdc396b1a
Ville Sillanpää <ville.sillanpaa@futurice.com>
2023-01-15 21:06:56 +0200

    Add colors and style social icons

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,34 @@ import githubOutline from "@iconify/icons-teenyicons/github-outline"
 import twitterOutline from "@iconify/icons-teenyicons/twitter-outline"
 import linkedinOutline from "@iconify/icons-teenyicons/linkedin-outline"
 
-import { Icon } from "@iconify/react"
+import { Icon, IconifyIcon } from "@iconify/react"
 import { graphql, useStaticQuery } from "gatsby"
 import React from "react"
 import { Link } from "./Link"
 
 import { css } from "@emotion/css"
+
+const LinkWithIcon = ({
+  link,
+  description,
+  icon,
+}: {
+  link: string
+  description: string
+  icon: IconifyIcon
+}) => (
+  <Link
+    href={link}
+    className={css`
+      display: flex;
+      gap: 4px;
+      align-items: center;
+    `}
+  >
+    <Icon icon={icon} height={20} />
+    <span>{description}</span>
+  </Link>
+)
 
 export const NavBar = () => {
   const data = useStaticQuery(graphql`
@@ -30,21 +52,33 @@ export const NavBar = () => {
     <div
       className={css`
         display: flex;
-        border-bottom: 2px solid black;
         justify-content: space-between;
+        background-color: #0006;
       `}
     >
       <span>Sillanpaa Dev</span>
-      <div>
-        <Link href={`https://github.com/${social.github}`}>
-          <Icon icon={githubOutline} />
-        </Link>
-        <Link href={`https://linkedin.com/in/${social.linkedin}`}>
-          <Icon icon={linkedinOutline} />
-        </Link>
-        <Link href={`https://twitter.com/${social.twitter}`}>
-          <Icon icon={twitterOutline} />
-        </Link>
+      <div
+        className={css`
+          display: flex;
+          gap: 12px;
+          padding: 12px;
+        `}
+      >
+        <LinkWithIcon
+          link={`https://github.com/${social.github}`}
+          description="Github"
+          icon={githubOutline}
+        />
+        <LinkWithIcon
+          link={`https://linkedin.com/in/${social.linkedin}`}
+          description="LinkedIn"
+          icon={linkedinOutline}
+        />
+        <LinkWithIcon
+          link={`https://twitter.com/${social.twitter}`}
+          description="Twitter"
+          icon={twitterOutline}
+        />
       </div>
     </div>
   )
